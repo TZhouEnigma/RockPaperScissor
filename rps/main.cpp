bb692ec06107b4e120f4822c70b40a1d1eeb58c6
TZhouEnigma <callmemj@bu.edu>
2017-07-14 03:09:28 -0400

    additional improvements

--- rps/main.cpp
+++ rps/main.cpp
@@ -8,6 +8,9 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
 
 using namespace std;
 
@@ -23,9 +26,12 @@ using namespace std;
 //
 //
 
+//Data used for randomizing RPS picker for testing purposes, generated using Wolfram Alpha
+int mat[100]={3, 1, 1, 3, 1, 2, 1, 1, 3, 2, 3, 2, 2, 1, 2, 1, 1, 1, 3, 1, 1, 2, 3, 1, 2, 3, 3, 1, 3, 3, 3, 3, 1, 2, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 2, 2, 1, 2, 1, 2, 3, 1, 3, 1, 2, 2, 3, 1, 2, 1, 3, 2, 3, 2, 3, 2, 2, 1, 3, 2, 2, 2, 2, 3, 1, 3, 2, 2, 2, 1, 2, 2, 2, 2, 1, 1, 1, 3, 2, 3, 3, 3, 2, 3, 1, 1, 2, 1, 2, 3};
 
 
 
+//Construct a HMM class
 class HMM{
     
 public:
@@ -73,45 +79,56 @@ void HMM::setSide(HMM &a,HMM &b){
     
 }
 
+
+// Decision function based on existing HMM
 char computer_Pick(HMM &R,HMM &P, HMM &S, char prev){
     char ans = '\0';
     
     if(prev == 'R'){
-      /*  cout<<"the probability of you picking"<<endl;
-        cout<<"Paper is:"<<R.Showleft()<<endl;
-        cout<<"Scissor is:"<<R.ShowRight()<<endl;
-        cout<<"Rock is:"<<R.ShowStay()<<endl;*/
         if(R.Showleft()>=R.ShowRight() && R.Showleft()>= R.ShowStay()) ans='S';
         if(R.ShowRight()>=R.Showleft() && R.ShowRight()>= R.ShowStay()) ans='R';
         if(R.ShowStay()>=R.ShowRight() && R.ShowStay()>= R.Showleft()) ans='P';
         
     }
     if(prev == 'P'){
-      /*  cout<<"the probability of you picking"<<endl;
-        cout<<"Paper is:"<<P.ShowStay()<<endl;
-        cout<<"Scissor:"<<P.Showleft()<<endl;
-        cout<<"Rock:"<<P.ShowRight()<<endl;
-      */
         if(P.Showleft()>=P.ShowRight() && P.Showleft()>= P.ShowStay()) ans='R';
         if(P.ShowRight()>=P.Showleft() && P.ShowRight()>= P.ShowStay()) ans='P';
         if(P.ShowStay()>=P.ShowRight() && P.ShowStay()>= P.Showleft()) ans='S';
         }
     if(prev == 'S'){
-        /*
-        cout<<"the probability of you picking"<<endl;
-        cout<<"Paper is:"<<S.ShowRight()<<endl;
-        cout<<"Scissor:"<<S.ShowStay()<<endl;
-        cout<<"Rock:"<<S.Showleft()<<endl;
-         */
+        
         if(S.Showleft()>=S.ShowRight() && S.Showleft()>= S.ShowStay()) ans='P';
         if(S.ShowRight()>=S.Showleft() && S.ShowRight()>= S.ShowStay()) ans='S';
         if(S.ShowStay()>=S.ShowRight() && S.ShowStay()>= S.Showleft()) ans='R';
     }
     
-    cout<<"Computer picked: "<<ans<<endl;
+    cout<<"Computer picked: "<<ans<<"\n "<<endl;
     
     return ans;
 }
+
+char result(char human,char ai){
+ if(human==ai)  return 'T';
+ else if((human=='R' && ai=='P') || (human=='P' && ai=='S') || (human=='S' && ai=='R')){
+     return 'L';
+ }
+   return 'W';
+    
+}
+
+
+//random RPS generator
+char randRPS(int in){
+    
+    int random = mat[in] ;
+    if(random == 1) return 'R';
+    if(random == 2) return 'P';
+    
+    return 'S';
+        
+}
+
+
 int main(){
     
     
@@ -123,26 +140,46 @@ int main(){
     Paper.setSide(Scissors, Rock);
     Scissors.setSide(Rock, Paper);
     
-    cout<<"hahahhh"<<endl;
-    Rock.Showleft();
     
     int numtries=100;
-    int count=0;
+    int count=1;
     char previous;
     
-    cout<<"Welcome to the challenge! \n Enter R,P OR S for wrap paper and scissors"<<endl;
+    cout<<"Welcome to the challenge! \nEnter R,P OR S for wrap, paper or scissors"<<endl;
     cin>>previous;//Initial state
     
+    double win=0;
+    double winOrLose=0;
     
     
     while(count<numtries){
         
-         computer_Pick(Rock, Paper, Scissors, previous);
-        
-    cout<<"Enter R,P OR S for wrap paper and scissors"<<endl;
+    
+  
         char Choose;
-        cin>>Choose;
+        
+      
+   //  UNCOMMENT the folloing two lines to play against computer
+  //   cout<<"Enter R,P OR S for wrap paper and scissors"<<endl;
+  //   cin>>Choose;
+   
+        Choose = randRPS(count);  //COMMENT out this line to play against computer
+        cout<<"Using randomized data,you picked "<<Choose<<endl; //COMMENT out this line to play against computer
+        char res= result(Choose, computer_Pick(Rock, Paper, Scissors, previous));
+        if(res=='W'){
+            ++win;
+            ++winOrLose;
+            cout<<"YOU WIN! \nYour winning percentage is now: "<< double(win/double(winOrLose))<<endl;
+        }
+        else if(res=='T'){
+            cout<<"TIE GAME! \nYour winning percentage is now: "<< double(win/double(winOrLose))<<endl;
+        }
+        else{
+            ++winOrLose;
+            cout<<"YOU LOSE! \nYour winning percentage is now: "<< double(win/double(winOrLose))<<endl;
+        }
        
+        
         
       //If the user choose the same as last round
         if(Choose==previous){
@@ -188,20 +225,7 @@ int main(){
     
     }
     
-   /*
-    cout<<"rock to paper:"<<Rock.Showleft()<<endl;
-    cout<<"rock to Scissor:"<<Rock.ShowRight()<<endl;
-    cout<<"rock to rock:"<<Rock.ShowStay()<<endl;
-    
-    
-    cout<<"paper to paper:"<<Paper.ShowStay()<<endl;
-    cout<<"paper to Scissor:"<<Paper.Showleft()<<endl;
-    cout<<"paper to rock:"<<Rock.ShowRight()<<endl;
-    
-    cout<<"scissors to paper:"<<Scissors.ShowRight()<<endl;
-    cout<<"scissors  to Scissor:"<<Scissors.ShowStay()<<endl;
-    cout<<"scissors  to rock:"<<Scissors.Showleft()<<endl;
-    */
+   
     return 0;
     
 }
